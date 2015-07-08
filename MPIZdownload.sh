555531f77c8030983c300a625363c7e88d115cf2
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2015-07-08 22:00:42 +0200

    tested and debugged

--- MPIZdownload.sh
+++ MPIZdownload.sh
@@ -6,11 +6,11 @@ key="$1"
 
 case $key in
     -l|--links_file)
-    f="$2"
+    f=${2}
     shift # past argument
     ;;
     -o|--output_folder)
-    folder="$(readlink -f ${2})"
+    folder=${2}
     shift # past argument
     ;;
     *)
@@ -19,12 +19,16 @@ esac
 shift # past argument or value
 done
 
-if [[ ! -d ${folder} ]]; then mkdir -p ${folder}; fi  
+f=$(readlink -f ${f})
+
+if [[ ! -e ${folder} ]]; then
+mkdir -p ${folder}; fi
+folder=$(readlink -f ${folder}) 
 
 cd ${folder}
 
-while read line; do down=${line:0:4}; if [[ ${down} == "http" ]]; then wget ${line}; fi; done < ${f};
-while read line; do md5=${line:0:6}; if [[ ${md5} == "md5sum"]]; then echo ${line:7} >> md5.tmp; fi; done < ${f}
+while read line; do down=${line:0:4}; if [[ ${down} == "http" ]]; then wget ${line}; fi; done < ${f}
+while read line; do md5=${line:0:6}; if [[ ${md5} == "md5sum" ]]; then echo ${line:7} >> md5.tmp; fi; done < ${f}
 md5sum -c md5.tmp 2>&1 | tee md5.log
 
 echo $(date) >> downloads.log
