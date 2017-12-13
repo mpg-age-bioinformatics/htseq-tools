0ab3073650d4e2d941628e618540614ddfe630ef
Jorge Boucas <jorgeboucas@users.noreply.github.com>
2017-12-13 08:12:04 +0100

    Add files via upload

--- QC.R
+++ QC.R
@@ -23,42 +23,49 @@ cuff<-readCufflinks()
 
 cuff
 
+print("dispersionPlot")
 disp<-dispersionPlot(genes(cuff))
 pdf(paste(output,"/dispersionPlot.pdf", sep=""))
 disp
 dev.off()
 rm(disp)
 
+print("fpkmSCVPlot")
 genes.scv<-fpkmSCVPlot(genes(cuff))
 pdf(paste(output,"/genes.scv.pdf", sep=""))
 genes.scv
 dev.off()
 rm(genes.scv)
 
+print("fpkmSCVPlot")
 isoforms.scv<-fpkmSCVPlot(isoforms(cuff))
 pdf(paste(output,"/isoforms.scv.pdf", sep=""))
 isoforms.scv
 dev.off()
 rm(isoforms.scv)
 
+print("csDensity")
 dens<-csDensity(genes(cuff))
 pdf(paste(output,"/csDensity.pdf",sep=""))
 dens
 dev.off()
 rm(dens)
 
+print("csDensity")
 densRep<-csDensity(genes(cuff),replicates=T)
 pdf(paste(output,"/csDensRep.pdf",sep=""))
 densRep
 dev.off()
 rm(densRep)
 
+print("csBoxplot")
 b<-csBoxplot(genes(cuff))
 pdf(paste(output,"/csBoxplot.pdf",sep=""))
 b
 dev.off()
 rm(b)
 
+print("csBoxplot") 
 brep<-csBoxplot(genes(cuff),replicates=T)
 # brep
 brepY<-brep+coord_cartesian(ylim=c(-5.15,5.15))
@@ -67,6 +74,7 @@ brepY
 dev.off()
 rm(brepY)
 
+print("csScatterMatrix")
 s<-csScatterMatrix(genes(cuff))
 pdf(paste(output,"/csScatterMatrix.pdf",sep=""))
 s
@@ -76,38 +84,45 @@ rm(s)
 # srep<-csScatterMatrix(genes(cuff),replicates=T)
 # srep
 
+print("csDendro")
 pdf(paste(output,"/csDendro.pdf",sep=""))
 dend<-csDendro(genes(cuff))
 dev.off()
 rm(dend)
 
+print("csDendro")
 pdf(paste(output,"/csDendrorep.pdf",sep=""))
 dend.rep<-csDendro(genes(cuff),replicates=T)
 dev.off()
 rm(dend.rep)
 
+print("PCAplot")
 genes.PCA<-PCAplot(genes(cuff),"PC1","PC2")
 pdf(paste(output,"/genes.PCA.pdf",sep=""))
 genes.PCA
 dev.off()
 rm(genes.PCA)
 
+print("PCAplot")
 genes.PCA.rep<-PCAplot(genes(cuff),"PC1","PC2",replicates=T)
 pdf(paste(output,"/genes.PCA.rep.pdf",sep=""))
 genes.PCA.rep
 dev.off()
 rm(genes.PCA.rep)
 
+print("MDSplot") 
 cond.PCoA <- MDSplot(genes(cuff))
 pdf(paste(output,"/cond.PCoA.pdf",sep=""))
 cond.PCoA
 dev.off()
 
+print("MDSplot") 
 cond.PCoA.rep <- MDSplot(genes(cuff), replicates = T)
 pdf(paste(output,"/cond.PCoA.rep.pdf",sep=""))
 cond.PCoA.rep
 dev.off()
 
+print("csHeatmap") 
 mySigGeneIds<-getSig(cuff,alpha=0.05,level='genes')
 mySigGenes<-getGenes(cuff,mySigGeneIds)
 
@@ -122,6 +137,7 @@ dev.off()
 #dev.off()
 rm(h)
 
+print("csHeatmap")
 h.rep<-csHeatmap(mySigGenes,cluster='both',replicates=T)
 h.rep$scales$scales[[2]]$labels <- sub("\\|.*", "",h.rep$scales$scales[[2]]$labels)
 h.rep$scales$scales[[2]]$labels <- sub(",.*", "",h.rep$scales$scales[[2]]$labels)
@@ -134,6 +150,7 @@ rm(h.rep)
 #dev.off()
 #rm(h.rep)
 
+print("sigMatrix")
 sigMatrix(cuff)
 pdf(paste(output,"/Significance_matrix.pdf",sep=""))
 sigMatrix(cuff)
